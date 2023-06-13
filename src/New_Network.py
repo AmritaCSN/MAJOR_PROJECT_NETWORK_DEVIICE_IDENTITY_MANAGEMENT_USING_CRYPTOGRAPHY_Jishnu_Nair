810db5d56986b226044099e9305d1b66668ce0ae
Dryseth <88357715+Dryseth@users.noreply.github.com>
2023-06-13 11:35:03 +0530

    New_Network.py

--- src/New_Network.py
+++ src/New_Network.py
@@ -73,7 +73,7 @@ with open("server_private_key.pem", "wb") as f:
 
 # Create a socket and listen for incoming connections
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("Ip Address of the Server", 8888))
+server_socket.bind(("192.168.0.105", 8888))
 server_socket.listen(1)
 
 print("Authentication Server is listening for connections...")
